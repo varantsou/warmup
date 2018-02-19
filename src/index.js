ec37854232a890935d49cc15bbb0007d38ebfffc
Valery Varantsou <voroncov2412@gmail.com>
2018-02-19 23:23:47 +0300

    solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+     switch (temperature) {
+          case - 20:
+               return -4
+               break;
+          case 0:
+               return 32
+               break;
+          case 15:
+               return 59
+               break;
+          case 40:
+               return 104
+               break;
+          default:
+               return false;
+     }
 };
